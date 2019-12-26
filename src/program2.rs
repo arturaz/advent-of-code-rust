10a5f21c32aa68f046925745914ac023346d70a1
Arturas Slajus <x11@arturaz.net>
2019-12-26 23:30:37 +0200

    WIP: 5-1

--- src/program2.rs
+++ src/program2.rs
@@ -44,9 +44,49 @@ fn computer_from_args(args: &mut Args) -> Result<Computer, String> {
         )
 }
 
-//mod computer {
-//    enum ParameterMode { Position, Immediate }
-//}
+#[derive(Eq, PartialEq)]
+enum OpCode { Add, Multiply, Halt, TakeInput, Output }
+impl OpCode {
+    fn parse(data: MemData) -> Result<OpCode, String> {
+        match data {
+            99 => Ok(OpCode::Halt),
+            1 => Ok(OpCode::Add),
+            2 => Ok(OpCode::Multiply),
+            3 => Ok(OpCode::TakeInput),
+            4 => Ok(OpCode::Output),
+            _ => Err(format!("Unknown op code {}", data))
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum ParameterMode { Position, Immediate }
+
+struct Instruction {
+    op_code: OpCode,
+    parameter_modes: Vec<ParameterMode>
+}
+impl Instruction {
+    fn parse(data: MemData) -> Result<Instruction, String> {
+        let op_code = OpCode::parse(data % 100)?;
+        let mut parameters_data = data / 100;
+        let mut parameter_modes = Vec::new();
+        while parameters_data > 0 {
+            let mode_res = match parameters_data % 10 {
+                0 => Ok(ParameterMode::Position),
+                1 => Ok(ParameterMode::Immediate),
+                other => Err(format!("Unknown parameter mode {} for parameter {}", other, parameter_modes.len())),
+            };
+            parameter_modes.push(mode_res?);
+            parameters_data /= 10;
+        }
+        Ok(Instruction { op_code, parameter_modes })
+    }
+
+    fn parameter_mode(&self, idx: usize) -> ParameterMode {
+        self.parameter_modes.get(idx).map_or_else(|| ParameterMode::Position, |r| *r)
+    }
+}
 
 #[derive(Clone)]
 struct Computer {
@@ -74,6 +114,15 @@ impl Computer {
         )
     }
 
+    pub fn get_param(&self, idx: usize, mode: ParameterMode) -> Result<&MemData, String> {
+        match mode {
+            ParameterMode::Position => {
+                self.get_idx(idx).and_then(|i| self.get(i))
+            },
+            ParameterMode::Immediate => self.get(idx),
+        }
+    }
+
     pub fn get_mut(&mut self, idx: usize) -> Result<&mut MemData, String> {
         if idx <= self.memory.len() - 1 { Ok(unsafe { self.memory.get_unchecked_mut(idx) }) }
         else { Err(self.oob_err(idx)) }
@@ -82,26 +131,27 @@ impl Computer {
     pub fn run(&mut self) -> Result<(), String> {
         let mut index = 0usize;
         loop {
-            let op_code = *self.get(index)?;
-            match op_code {
-                99 => return Ok(()),
-                1 | 2 => {
-                    let a_idx = self.get_idx(index + 1)?;
-                    let b_idx = self.get_idx(index + 2)?;
+            let instruction = Instruction::parse(*self.get(index)?)?;
+            let get_param = |idx: u8| {
+                self.get_param(index + idx as usize + 1, instruction.parameter_mode(idx.into()))
+            };
+
+            match instruction.op_code {
+                OpCode::Halt => return Ok(()),
+                OpCode::Add | OpCode::Multiply => {
+                    let a = *get_param(0)?;
+                    let b = *get_param(1)?;
                     let result_idx = self.get_idx(index + 3)?;
-                    let a = *self.get(a_idx)?;
-                    let b = *self.get(b_idx)?;
                     let result = self.get_mut(result_idx)?;
-                    *result = if op_code == 1 { a + b } else { a * b };
+                    *result = if instruction.op_code == OpCode::Add { a + b } else { a * b };
                     index += 4;
                 },
-                3 => {
-                    unimplemented!()
+                OpCode::TakeInput => {
+                    index += 2;
                 },
-                4 => {
-                    unimplemented!()
-                },
-                other => return Err(format!("Unknown op code: {}", other))
+                OpCode::Output => {
+                    index += 2;
+                }
             }
         }
     }
